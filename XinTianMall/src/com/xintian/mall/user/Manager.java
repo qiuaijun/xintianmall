a34694251cfa11d92f82d5aee3fece3800ea7c3b
qiuaijun <qiuaijun@126.com>
2015-04-26 10:04:03 +0800

    新增管理员用户

---
+++ XinTianMall/src/com/xintian/mall/user/Manager.java
@@ -0,0 +1,20 @@
+package com.xintian.mall.user;
+
+/**
+ * 管理员用户
+ * 
+ * @author qiuaijun
+ *
+ * @date 2015年4月26日
+ */
+public class Manager {
+
+	private long id;
+	// 用户登陆名称
+	private String loinName;
+	// 用户的密码
+	private String pwd;
+	// 用户的真实名称
+	private String name;
+
+}
